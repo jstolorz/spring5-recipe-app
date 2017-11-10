c7bf4d9bda43bde9a66b33df09a66b9a13aa9c8a
Janusz Stolorz <jstolorz@gmail.com>
2017-11-10 14:51:51 +0100

    Adding population database

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -12,7 +12,7 @@ public class UnitOfMeasure {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String uom;
+    private String description;
 
 
     public Long getId() {
@@ -24,18 +24,18 @@ public class UnitOfMeasure {
     }
 
     public String getUom() {
-        return uom;
+        return description;
     }
 
     public void setUom(String uom) {
-        this.uom = uom;
+        this.description = uom;
     }
 
     @Override
     public String toString() {
         return "UnitOfMeasure{" +
                 "id=" + id +
-                ", uom='" + uom + '\'' +
+                ", uom='" + description + '\'' +
                 '}';
     }
 }
