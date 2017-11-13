f5ae2666400b5cac53d1c021e37c938f557381b5
Janusz Stolorz <jstolorz@gmail.com>
2017-11-13 13:18:22 +0100

    Adding check of repositories and controller to index

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,14 +1,35 @@
 package guru.springframework.controllers;
 
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    CategoryRepository categoryRepository;
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"","/","/index"})
     public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("Italian");
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Cup");
+
+        System.out.println("Category Id : " + categoryOptional.get().getId());
+        System.out.println("Unit Id : " + unitOfMeasure.get().getId());
+
         //System.out.println("olo Yooo");
         return "index";
     }
